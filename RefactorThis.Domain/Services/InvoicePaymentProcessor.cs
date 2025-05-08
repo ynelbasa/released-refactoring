361bbfbe909eb31029f97d5311ca97fc98482f8c
Ynel Basa <marinelbasa@gmail.com>
2025-05-08 17:54:22 +1200

    Add dictionary lookup for payment calculator and replace switch conditions; Remove duplicated calculations

--- RefactorThis.Domain/Services/InvoicePaymentProcessor.cs
+++ RefactorThis.Domain/Services/InvoicePaymentProcessor.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using RefactorThis.Domain.Entities;
 using RefactorThis.Domain.Enums;
@@ -10,6 +11,20 @@ namespace RefactorThis.Domain.Services
     public class InvoicePaymentProcessor
     {
         private readonly IInvoiceRepository _invoiceRepository;
+
+        private readonly Dictionary<InvoiceType, Action<Payment, Invoice>> _succeedingPaymentCalculators =
+            new Dictionary<InvoiceType, Action<Payment, Invoice>>
+            {
+                { InvoiceType.Standard, CalculateStandardInvoiceSucceedingPayment },
+                { InvoiceType.Commercial, CalculateCommercialInvoiceSucceedingPayment }
+            };
+
+        private readonly Dictionary<InvoiceType, Action<Payment, Invoice>> _initialPaymentCalculators =
+            new Dictionary<InvoiceType, Action<Payment, Invoice>>
+            {
+                { InvoiceType.Standard, CalculateInitialPayment },
+                { InvoiceType.Commercial, CalculateInitialPayment }
+            };
 
         public InvoicePaymentProcessor(IInvoiceRepository invoiceRepository)
         {
@@ -46,85 +61,21 @@ namespace RefactorThis.Domain.Services
             return responseMessage;
         }
 
-        private static string ProcessSucceedingPayment(Payment payment, Invoice invoice)
+        private string ProcessSucceedingPayment(Payment payment, Invoice invoice)
         {
-            string responseMessage;
-            if ((invoice.Amount - invoice.AmountPaid) == payment.Amount)
-            {
-                switch (invoice.Type)
-                {
-                    case InvoiceType.Standard:
-                        invoice.AmountPaid += payment.Amount;
-                        invoice.Payments.Add(payment);
-                        responseMessage = "final partial payment received, invoice is now fully paid";
-                        break;
-                    case InvoiceType.Commercial:
-                        invoice.AmountPaid += payment.Amount;
-                        invoice.TaxAmount += payment.Amount * 0.14m;
-                        invoice.Payments.Add(payment);
-                        responseMessage = "final partial payment received, invoice is now fully paid";
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException();
-                }
-            }
-            else
-            {
-                switch (invoice.Type)
-                {
-                    case InvoiceType.Standard:
-                        invoice.AmountPaid += payment.Amount;
-                        invoice.Payments.Add(payment);
-                        responseMessage = "another partial payment received, still not fully paid";
-                        break;
-                    case InvoiceType.Commercial:
-                        invoice.AmountPaid += payment.Amount;
-                        invoice.TaxAmount += payment.Amount * 0.14m;
-                        invoice.Payments.Add(payment);
-                        responseMessage = "another partial payment received, still not fully paid";
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException();
-                }
-            }
-
+            var isFinalPayment = (invoice.Amount - invoice.AmountPaid) == payment.Amount;
+            _succeedingPaymentCalculators[invoice.Type](payment, invoice);
+            var responseMessage = isFinalPayment
+                ? "final partial payment received, invoice is now fully paid"
+                : "another partial payment received, still not fully paid";
             return responseMessage;
         }
 
-        private static string ProcessInitialPayment(Payment payment, Invoice invoice)
+        private string ProcessInitialPayment(Payment payment, Invoice invoice)
         {
-            string responseMessage;
-            if (invoice.Amount == payment.Amount)
-            {
-                switch (invoice.Type)
-                {
-                    case InvoiceType.Standard:
-                    case InvoiceType.Commercial:
-                        invoice.AmountPaid = payment.Amount;
-                        invoice.TaxAmount = payment.Amount * 0.14m;
-                        invoice.Payments.Add(payment);
-                        responseMessage = "invoice is now fully paid";
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException();
-                }
-            }
-            else
-            {
-                switch (invoice.Type)
-                {
-                    case InvoiceType.Standard:
-                    case InvoiceType.Commercial:
-                        invoice.AmountPaid = payment.Amount;
-                        invoice.TaxAmount = payment.Amount * 0.14m;
-                        invoice.Payments.Add(payment);
-                        responseMessage = "invoice is now partially paid";
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException();
-                }
-            }
-
+            var isFullPayment = invoice.Amount == payment.Amount;
+            _initialPaymentCalculators[invoice.Type](payment, invoice);
+            var responseMessage = isFullPayment ? "invoice is now fully paid" : "invoice is now partially paid";
             return responseMessage;
         }
 
@@ -135,6 +86,26 @@ namespace RefactorThis.Domain.Services
 
             if (invoice.Amount == 0 && invoice.Payments != null)
                 throw new InvalidInvoiceStateException();
+        }
+
+        private static void CalculateInitialPayment(Payment payment, Invoice invoice)
+        {
+            invoice.AmountPaid = payment.Amount;
+            invoice.TaxAmount = payment.Amount * 0.14m;
+            invoice.Payments.Add(payment);
+        }
+
+        private static void CalculateStandardInvoiceSucceedingPayment(Payment payment, Invoice invoice)
+        {
+            invoice.AmountPaid += payment.Amount;
+            invoice.Payments.Add(payment);
+        }
+
+        private static void CalculateCommercialInvoiceSucceedingPayment(Payment payment, Invoice invoice)
+        {
+            invoice.AmountPaid += payment.Amount;
+            invoice.TaxAmount += payment.Amount * 0.14m;
+            invoice.Payments.Add(payment);
         }
     }
 }
