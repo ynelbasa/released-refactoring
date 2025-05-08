1f576877ef03b38fdfc7e7406c85e61fe836b78c
Ynel Basa <marinelbasa@gmail.com>
2025-05-08 17:23:02 +1200

    Add invoice processing exceptions

---
+++ RefactorThis.Domain/Exceptions/InvalidInvoiceTypeException.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RefactorThis.Domain.Exceptions
+{
+    public class InvalidInvoiceTypeException : ArgumentOutOfRangeException
+    {
+    }
+}
---
+++ RefactorThis.Domain/Exceptions/MissingInvoiceException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace RefactorThis.Domain.Exceptions
+{
+    public class MissingInvoiceException : InvalidOperationException
+    {
+        public MissingInvoiceException() : base("There is no invoice matching this payment")
+        {
+        }
+    }
+}
